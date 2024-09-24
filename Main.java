157147bd04213b8e051e0723a99c932fce5bd73e
Edin <edin.licina.2@gmail.com>
2024-09-24 18:52:05 +0200

    Added first endpoint

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main{
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello world!");
+        InetSocketAddress socketAddress = new InetSocketAddress(10000);
+        HttpServer server = HttpServer.create(socketAddress, 0);
+        server.createContext("/hello-world", new HelloWorldHandler());
+        server.start();
     }
 }
